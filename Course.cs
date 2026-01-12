02339167b996d3306d17aa34898c19370911241f
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2026-01-12 18:00:28 +0000

    why is MaxStudents orange?

--- Course.cs
+++ Course.cs
@@ -2,9 +2,9 @@
 public class Course
 {   
     // Add your code here
-    public int maxStudents = 25;
+    public int MaxStudents = 25;
 
-    public const int perfectScore = 100;
+    public const int PerfectScore = 100;
     public int CourseLength(){
         // Add your code here
         int courseLength = 20;
